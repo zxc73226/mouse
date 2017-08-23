2af642ef4434b751f85311449e2d1a3df22c630a
DESKTOP-FJPDUTL\user <zxc73226>
2017-08-23 18:25:07 +0800

    進度更新

--- js/mouse.js
+++ js/mouse.js
@@ -1,29 +1,63 @@
+var colors = ["Red", "Orange", "Yellow", "Green", "Blue", "Indigo", "Purple"];
+let randColor = (a, r, g, b) => {
+  if (typeof a != "number") a = 0.7;
+  if (typeof r != "number") r = ~~(Math.random() * 255);
+  if (typeof g != "number") g = ~~(Math.random() * 255);
+  if (typeof b != "number") b = ~~(Math.random() * 255);
+  return `rgba(${r},${g},${b},${a})`;
+  //let color = 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+  //console.log(color);
+  return color;
+}
+
+let xyPool = [];
+let colorPool = [];
+let nowX = 0;
+let nowY = 0;
 $(document).ready(function () {
-  $(window).mousemove(function (evt) {
-    var x = evt.pageX;
-    var y = evt.pageY;
-    console.log(x + "," + y);
-    $(".block").each((i, el) => {
-      $(el).css("left", x + "px");
-      $(el).css("top", y + "px");
+  let $blocks = $('.block');
+  let $window = $(window);
+  let $mousePosition = $("#mousePosition");
+
+  //每0.5秒更換一次顏色
+  setInterval(() => {
+    colorPool.unshift(randColor());
+    colorPool.forEach((color, i) => {
+      $("#box" + i).css("color", color);
+    })
+    colorPool.length = $blocks.length;
+  }, 500);
+
+  //每0.033秒取樣一次
+  setInterval(() => {
+    xyPool.unshift({
+      x: nowX,
+      y: nowY
     });
+    //寫入每個位置
+    xyPool.forEach((pos, i) => {
+      $("#box" + i).css({
+        left: pos.x + "px",
+        top: pos.y + "px"
+      })
+    })
+    xyPool.length = $blocks.length;
+  }, 33)
 
-    for (var j = 1; j < 8; j++) {
-      $("#box" + [j]).each((i, ey) => {
-        setTimeout(function () {
-          $(ey).css("left", x + "px");
-          $(ey).css("top", y + "px");
-        }, j * 150);
-      });
-    }
-
-    var r = Math.floor(Math.random() * 255);
-    var g = Math.floor(Math.random() * 255);
-    var b = Math.floor(Math.random() * 255);
-
-    $(".block").css('color', 'rgba(' + r + ',' + g + ',' + b + ',)');
-
-    document.querySelector("#mousePosition").innerHTML = x + "," + y;
+  //即時監控滑鼠位置
+  $window.mousemove(function (evt) {
+    nowX = evt.pageX + 15;
+    nowY = evt.pageY + 15;
+    $mousePosition.html(nowX + "," + nowY);
   });
-  console.log(JSON.parse(document.querySelector("#mousePosition")));
+
+  //初始化
+  $blocks.each(() => {
+    xyPool.push({
+      X: 0,
+      y: 0
+    });
+    colorPool.push(randColor());
+  });
+
 });
