3d4df24c958e3e7318b912cfb630081ca6c537af
DESKTOP-FJPDUTL\user <zxc73226>
2017-08-30 18:43:57 +0800

    js 進度更新

--- js/mouses.js
+++ js/mouses.js
@@ -60,4 +60,18 @@ $(document).ready(function () {
     });
     colors.push(randomColor());
   });
+
+  //顯示時間
+  function current() {
+    var d = new Date(), str = '';
+    str += d.getFullYear() + '年'; //獲取當前年份 
+    str += d.getMonth() + 1 + '月'; //獲取當前月份（0——11） 
+    str += d.getDate() + '日';
+    str += d.getHours() + '時';
+    str += d.getMinutes() + '分';
+    str += d.getSeconds() + '秒';
+    return str;
+  }
+  setInterval(function () { $("#nowTime").html(current) }, 1000);
+
 });
